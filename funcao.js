7464d313f5f671e84bbf8386b19c58cafa614661
Lucas Araujo <l.araujo000@gmail.com>
2023-10-21 20:39:54 -0300

    Função de verificar maior data atualizada p/ timestamp

--- funcao.js
+++ funcao.js
@@ -1,15 +1,8 @@
 function ehMaior(data1 = new Date(), dataAgora = new Date()) {
 
-    if (data1.getFullYear() < dataAgora.getFullYear()) {
-        return dataAgora;
-    } else if (data1.getFullYear() == dataAgora.getFullYear() && data1.getMonth() > dataAgora.getMonth()) {
-        return data1;
-    } else if (data1.getFullYear() == dataAgora.getFullYear() && data1.getMonth() < dataAgora.getMonth()) {
-        return dataAgora;  
-    } else if (data1.getFullYear() == dataAgora.getFullYear() && data1.getMonth() == dataAgora.getMonth() && dataAgora.getDate() > data1.getDate()) {
+    if (data1.getTime() < dataAgora.getTime()) {
         return dataAgora;
     } else {
         return data1;
     }
 }
-        
